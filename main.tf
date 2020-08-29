38d8289339583d39c76f5f721ab9b833829b0878
Pietruszka, Michal <mike.pietruszka@transunion.com>
2020-08-29 15:11:44 -0500

    Testing variables

--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
+variable "cidr_block" {
+  type        = string
+  description = "VPC CIDR block"
+}
 resource "aws_vpc" "main" {
   cidr_block       = var.cidr_block
   instance_tenancy = "default"
